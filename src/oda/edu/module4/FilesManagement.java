d413cef3995b5694480c2c77c4ba96c922f61acf
Denys <forestssssssss@gmail.com>
2021-04-12 16:00:44 +0300

    module4 - task1

---
+++ src/oda/edu/module4/FilesManagement.java
@@ -0,0 +1,41 @@
+package oda.edu.module4;/*
+  @author   Denys Ovdiienko
+  @project   oda.edu
+  @class  FilesManagement
+  @version  1.0.0 
+  @since 12.04.2021 - 15.32
+*/
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class FilesManagement {
+    public static void main(String[] args) throws IOException {
+        LocalDateTime start = LocalDateTime.now();
+        String logs = new String(Files.readAllBytes(Paths.get("D:\\logs.txt")));
+        final String [] lines = logs.split("\n");
+        System.out.println( "total number of logs(lines): " + lines.length);
+        System.out.println("-----------Use previous skills - String.split--------------------------");
+        LocalDateTime finish = LocalDateTime.now();
+        System.out.println("Time spent : " + ChronoUnit.MILLIS.between(start,finish) + " mss");
+
+
+        int counter = 0;
+        for (int i = 0; i <lines.length ; i++) {
+            if(lines[i].contains("ERROR")) counter++;
+        }
+        System.out.println("Number of errors:   " + counter);
+        System.out.println("-------------- Use Files.lines.-----------------");
+
+        start = LocalDateTime.now();
+        System.out.println("Number of errors:   " + Files.lines(Paths.get("D:\\logs.txt"))
+                .filter(element -> element.contains("ERROR")).count());
+
+
+        finish = LocalDateTime.now();
+        System.out.println("Time spent : " +ChronoUnit.MILLIS.between(start,finish) + "  mss");
+    }
+}
